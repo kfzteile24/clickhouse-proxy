6f13d617e567df858a4c159e2e0b65c7c97ee375
Alexandru Pisarenco <pisarenco.a@gmail.com>
2019-05-06 09:28:48 +0200

    Simplifier that respects operator precedence

--- simplifier.py
+++ simplifier.py
@@ -1,4 +1,4 @@
-import sqlparse
+import sqlparse, operator
 
 class Operand:
     """ Wrapper for any operand made for str(). If it's a token from sqlparse, then use the `normalized` attribute
@@ -17,26 +17,34 @@ class Operand:
         return str(self.value)
 
 
-class Operation:
-    """ Operation (operator) class that takes a list of arguments, and tries to evaluate it. This is the base class.
+class Operator:
+    """ Operator (operator) class that takes a list of arguments, and tries to evaluate it. This is the base class.
     The evaluated expression will either be a simplified form (scalar) or the expression itself if it can't be
     simplified. Simplification is done using the str() conversion.
     """
     def __init__(self):
         self.operands = []
-        # Is either binary or unknown
-        self.is_unary = False
 
+    @property
+    def precendence(self):
+        raise Exception(f"precedence not implemented for type {type(self).__name__}")
 
     def __repr__(self):
-        return f'<Operation "{type(self).__name__}">'
+        return f'<Operator "{type(self).__name__}">'
 
+    @property
+    def canBeUnary(self):
+        return False
+
+    @property
+    def can_be_binary(self):
+        return True
 
     def hasEnoughOperands(self, operands):
-        return (self.is_unary and len(perands) == 1) or (len(operands) == 2)
+        return (self.canBeUnary and len(operands) == 1) or (len(operands) == 2)
 
 
-class Is(Operation):
+class Is(Operator):
     def __str__(self):
         o0 = str(self.operands[0])
         o1 = str(self.operands[1])
@@ -58,15 +66,23 @@ class Is(Operation):
 
         return f'({o0}) IS ({o1})'
 
+    @property
+    def precedence(self):
+        return 4
 
-class In(Operation):
+
+class In(Operator):
     def __str__(self):
         o0 = str(self.operands[0])
         o1 = str(self.operands[1])
         return f'({o0}) IN ({o1})'
 
+    @property
+    def precedence(self):
+        return 7
 
-class Or(Operation):
+
+class Or(Operator):
     def __str__(self):
         o0 = str(self.operands[0])
         o1 = str(self.operands[1])
@@ -80,8 +96,12 @@ class Or(Operation):
             return o0
         return f'({o0}) OR ({o1})'
 
+    @property
+    def precedence(self):
+        return 7
 
-class And(Operation):
+
+class And(Operator):
     def __str__(self):
         o0 = str(self.operands[0])
         o1 = str(self.operands[1])
@@ -95,8 +115,12 @@ class And(Operation):
             return o0
         return f'({o0}) AND ({o1})'
 
+    @property
+    def precedence(self):
+        return 6
 
-class Not(Operation):
+
+class Not(Operator):
     def hasEnoughOperands(self):
         return len(self.operands) == 1
 
@@ -108,12 +132,15 @@ class Not(Operation):
             return 'TRUE'
         return f'NOT ({o})'
 
+    @property
+    def precedence(self):
+        return 5
+
 
 class Expression:
     def __init__(self, expression):
         self.expr = sqlparse.parse(expression)[0]
         self.simplified = self.parse(self.expr)
-
 
     def parse(self, statement):
         """ Parses the token list / tree tries to identify operators and operands, and bind them together.
@@ -153,19 +180,43 @@ class Expression:
             elif isinstance(tokens[tindex], sqlparse.sql.Identifier) \
                 or isinstance(tokens[tindex], sqlparse.sql.Comparison):
                 expression_elements.append(Operand(tokens[tindex]))
-            #    operands.append(Operand(tokens[tindex]))
-            #    if operation is not None:
-            #        operation.is_unary = True
-            
-
-            #if operation is not None and operation.hasEnoughOperands(operands):
-            #    operation.operands = operands
-            #    operands = [Operand(operation)]
-            #    operation = None
 
             tindex += 1
         return expression_elements
         #return str(operands[0])
+
+    def evaluate(self, expression_elements):
+        '''Evaluate an expression expressed as a list of operators and operands, based on operator precedence
+        '''
+        # We need to remember the order of the operators in the original expression even after we sort them according
+        # to precedence
+        index_preserved_elements = enumerate(expression_elements)
+        operators_only = [o for o in index_preserved_elements if isinstance(o, Operator)]
+        # sort according to precedence first, then according to natural order
+        sorted_operators = sorted([list(t) for t in enumerate(expression_elements)],
+                                  key=lambda e: (e[1].precendence, e[0]))
+        result = None
+        for index, op in sorted_operators:
+            if index==0 or not op.can_be_binary:
+                # is definitely unary
+                operand_ids = [index-1]
+            else:
+                operand_ids = [index-1, index+1]
+            # if overflow is detected, then we have a wrong expression
+            op.operands = [expression_elements[i] for i in operand_ids]
+            result = Operand(str(op))
+            # Update operator with its actual value
+            expression_elements[index] = result
+            # remove operands that have been processed into the above result
+            # take care of all indexes as well
+            # TODO: This would probably be prettier with a double-linked list
+            for opindex in operand_ids[::-1]:
+                del expression_elements[opindex]
+                # Also update all bigger indexes in sorted_operators since those changed
+                for op in sorted_operators:
+                    if op[0] > opindex:
+                        op[0] -= 1
+        return result
 
 
 if __name__ == '__main__':
